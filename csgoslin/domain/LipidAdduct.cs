80ad15cb5e998934139ac4c45926bf30d188418a
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2021-08-03 14:09:32 +0200

    initial commit

---
+++ csgoslin/domain/LipidAdduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csgoslin
+{
+    public class LipidAdduct
+    {
+        public LipidAdduct(){
+
+        }
+        
+        public bool StartsWithUpper(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
