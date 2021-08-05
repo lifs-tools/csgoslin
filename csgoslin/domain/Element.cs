b91edaa1bc143d59d3cd20cb5cb7073ef3ea5ce0
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2021-08-05 07:46:08 +0200

    fixed domain/Elements

--- csgoslin/domain/Element.cs
+++ csgoslin/domain/Element.cs
@@ -27,24 +27,24 @@ SOFTWARE.
 using System;
 using System.Text;
 using System.Collections.Generic;
-using ElementTable = System.Collections.Generic.Dictionary<Element, int>;
+//using ElementTable = System.Collections.Generic.Dictionary<Element, int>;
 
 namespace csgoslin
 {
-    public class Element
+    public class Elements
     {
         public enum Element {C, C13, H, H2, N, N15, O, O17, O18, P, P32, S, S34, S33, F, Cl, Br, I, As};
 
-        static Dictionary<string, Element> element_positions = new Dictionary<string, Element>{{"C", C}, {"H", H}, {"N", N}, {"O", O}, {"P", P}, {"P'", P32}, {"S", S}, {"F", F}, {"Cl", Cl}, {"Br", Br}, {"I", I}, {"As", As}, {"S'", S34}, {"S''", S33}, {"H'", H2}, {"C'", C13}, {"N'", N15}, {"O'", O17}, {"O''", O18}, {"2H", H2}, {"13C", C13}, {"15N", N15}, {"17O", O17}, {"18O", O18}, {"32P", P32}, {"34S", S34}, {"33S", S33}, {"H2", H2}, {"C13", C13}, {"N15", N15}, {"O17", O17}, {"O18", O18}, {"P32", P32}, {"S34", S34}, {"S33", S33}};
+        
+        public static Dictionary<string, Element> element_positions = new Dictionary<string, Element>{{"C", Element.C}, {"H", Element.H}, {"N", Element.N}, {"O", Element.O}, {"P", Element.P}, {"P'", Element.P32}, {"S", Element.S}, {"F", Element.F}, {"Cl", Element.Cl}, {"Br", Element.Br}, {"I", Element.I}, {"As", Element.As}, {"S'", Element.S34}, {"S''", Element.S33}, {"H'", Element.H2}, {"C'", Element.C13}, {"N'", Element.N15}, {"O'", Element.O17}, {"O''", Element.O18}, {"2H", Element.H2}, {"13C", Element.C13}, {"15N", Element.N15}, {"17O", Element.O17}, {"18O", Element.O18}, {"32P", Element.P32}, {"34S", Element.S34}, {"33S", Element.S33}, {"H2", Element.H2}, {"C13", Element.C13}, {"N15", Element.N15}, {"O17", Element.O17}, {"O18", Element.O18}, {"P32", Element.P32}, {"S34", Element.S34}, {"S33", Element.S33}};
+        
+
+        public static  Dictionary<Element, double> element_masses = new Dictionary<Element, double>{{Element.C, 12.0},  {Element.H, 1.007825035},  {Element.N, 14.0030740}, {Element.O, 15.99491463}, {Element.P, 30.973762},  {Element.S, 31.9720707}, {Element.H2, 2.014101779},  {Element.C13, 13.0033548378},  {Element.N15, 15.0001088984}, {Element.O17, 16.9991315}, {Element.O18, 17.9991604}, {Element.P32, 31.973907274}, {Element.S33, 32.97145876}, {Element.S34, 33.96786690}, {Element.F, 18.9984031}, {Element.Cl, 34.968853}, {Element.Br, 78.918327}, {Element.I, 126.904473}, {Element.As, 74.921595}};
 
 
-        static Dictionary<Element, double> element_masses = new Dictionary<Element, double>{{C, 12.0},  {H, 1.007825035},  {N, 14.0030740}, {O, 15.99491463}, {P, 30.973762},  {S, 31.9720707}, {H2, 2.014101779},  {C13, 13.0033548378},  {N15, 15.0001088984}, {O17, 16.9991315}, {O18, 17.9991604}, {P32, 31.973907274}, {S33, 32.97145876}, {S34, 33.96786690}, {F, 18.9984031}, {Cl, 34.968853}, {Br, 78.918327}, {I, 126.904473}, {As, 74.921595}};
+        
+        public static  Dictionary<Element, string> element_shortcut = new Dictionary<Element, string>{{Element.C, "C"}, {Element.H, "H"}, {Element.N, "N"}, {Element.O, "O"}, {Element.P, "P"}, {Element.S, "S"}, {Element.F, "F"}, {Element.Cl, "Cl"}, {Element.Br, "Br"}, {Element.I, "I"}, {Element.As, "As"}, {Element.H2, "H'"}, {Element.C13, "C'"}, {Element.N15, "N'"}, {Element.O17, "O'"}, {Element.O18, "O''"}, {Element.P32, "P'"}, {Element.S33, "S'"}, {Element.S34, "S''"}};
 
-
-
-        const Dictionary<Element, string> element_shortcut = new Dictionary<Element, string>{{C, "C"}, {H, "H"}, {N, "N"}, {O, "O"}, {P, "P"}, {S, "S"}, {F, "F"}, {Cl, "Cl"}, {Br, "Br"}, {I, "I"}, {As, "As"}, {H2, "H'"}, {C13, "C'"}, {N15, "N'"}, {O17, "O'"}, {O18, "O''"}, {P32, "P'"}, {S33, "S'"}, {S34, "S''"}};
-
-        static List<Element> element_order = {C, H, As, Br, Cl, F, I, N, O, P, S, H2, C13, N15, O17, O18, P32, S33, S34};
-        }
+        public static List<Element> element_order = new List<Element>{Element.C, Element.H, Element.As, Element.Br, Element.Cl, Element.F, Element.I, Element.N, Element.O, Element.P, Element.S, Element.H2, Element.C13, Element.N15, Element.O17, Element.O18, Element.P32, Element.S33, Element.S34};
     }
 }
